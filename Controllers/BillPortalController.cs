0356e5a6be525413183cb08f6082e575c9598bd1
unknown <khairnarabhi25@gmail.com>
2023-05-10 11:30:28 +0530

    Registring user

---
+++ Controllers/BillPortalController.cs
@@ -0,0 +1,92 @@
+﻿using Azure.Core;
+using Billing_Management_System.Data;
+using Billing_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace Billing_Management_System.Controllers
+{
+	public class BillPortalController : Controller
+	{
+		private readonly AppDbContext _context;
+        public BillPortalController(AppDbContext context)
+        {
+			_context = context;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(IFormCollection frm)
+		{
+			string userID = frm["email"];
+			string password = frm["Pwd"];
+
+			UserModel user = _context.Users.Find(userID);
+			if (user != null && user.Password == password)
+			{
+				return RedirectToAction("User");
+			}
+			else
+			{
+				ViewData["ErrorFlag"] = "Invalid Email or Password!";
+				return View("Index");
+			}
+		}
+
+		[HttpGet]
+		public IActionResult Bill()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Bill_Listing()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult User()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Vendor_Bill()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Vendor_Master()
+		{
+			return View();
+		}
+
+		
+		[HttpPost]
+		public IActionResult RegisterUser(UserModel user)
+		{
+			UserModel emailExists = _context.Users.Find(user.Email);
+			if(emailExists != null)
+			{
+				
+			}
+			if (ModelState.IsValid)
+			{
+				_context.Users.Add(user);
+				_context.SaveChanges();
+				return View("User");
+			}
+			return Redirect("User");
+		}
+	}
+}
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -4,14 +4,21 @@ namespace Billing_Management_System.Models
 {
     public class UserModel
     {
+        [Required(ErrorMessage ="Email is mandatory")]
+        [RegularExpression("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$",ErrorMessage ="Invalid Email address")]
         [Key]
         public String Email { get; set; }
-        [Required(ErrorMessage ="Email is Required")]
-        public String FullName { get; set; }
-        [Required(ErrorMessage = "Name is Required")]
-        public String Password { get; set; }
-        [Required(ErrorMessage = "Password is Required")]
+
+		[Required(ErrorMessage = "Full Name is Required")]
+		public String FullName { get; set; }
+
+        [StringLength(maximumLength:15,MinimumLength =6)]
+		[Required(ErrorMessage = "Password is Required")]
+		public String Password { get; set; }
+
         public String Role { get; set; }
+
+        //[Required(ErrorMessage ="Please Select the department")]
         public String Department { get; set; }
         public String ReportingTo { get; set; }
     }
